1976f8b1730baf607276434714ff65115775a8a7
Jessica P Sousa <93776215+JessicaPersou@users.noreply.github.com>
2025-07-15 21:34:21 -0300

    refactor: remove dependency
    * test: implementing test
    
    * refactor: remove integration test dependency

--- src/main/java/com/persou/prontosus/ProntosusApplication.java
+++ src/main/java/com/persou/prontosus/ProntosusApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProntosusApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProntosusApplication.class, args);
     }
-
 }
